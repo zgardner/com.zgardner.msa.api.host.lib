fbf16ce0d25b9fc987dfd5b70ec05a985ae7f71d
Zach Gardner <zgardner@keyholesoftware.com>
2015-08-09 10:08:29 -0500

    Initial import from the maven project quick start

---
+++ src/main/java/com/zgardner/msa/api/host/lib/App.java
@@ -0,0 +1,13 @@
+package com.zgardner.msa.api.host.lib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
